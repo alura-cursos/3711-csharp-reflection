e125baa63fddebbdbaab7025bcd60772b62cd35d
marcelooliveira <mclricardo@gmail.com>
2024-03-10 00:32:16 -0300

    3711-csharp-reflection-video3.1-investigando-construtores.mov

--- ByteBank.ConsoleApp/Program.cs
+++ ByteBank.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank.Common;
+using System.Reflection.Emit;
 
 MostrarBanner();
 
@@ -79,6 +80,26 @@ static void GravarGrupoBoletos()
     var leitorDeBoleto = new LeitorDeBoleto();
     List<Boleto> boletos = leitorDeBoleto.LerBoletos("Boletos.csv");
 
-    RelatorioDeBoleto gravadorDeCSV = new RelatorioDeBoleto("BoletosPorCedente.csv");
-    gravadorDeCSV.Processar(boletos);
+    //RelatorioDeBoleto gravadorDeCSV = new RelatorioDeBoleto("BoletosPorCedente.csv");
+    //gravadorDeCSV.Processar(boletos);
+
+    var nomeParametroConstrutor = "nomeArquivoSaida";
+    var parametroConstrutor = "BoletosPorCedente.csv";
+    var nomeMetodo = "Processar";
+    var parametroMetodo = boletos;
+
+    ProcessarDinamicamente(nomeParametroConstrutor, parametroConstrutor,
+        nomeMetodo, parametroMetodo);
+}
+
+static void ProcessarDinamicamente(string nomeParametroConstrutor, string parametroConstrutor, string nomeMetodo, List<Boleto> parametroMetodo)
+{
+    var tipoClasseRelatorio = typeof(RelatorioDeBoleto);
+    var construtores = tipoClasseRelatorio.GetConstructors();
+
+    foreach (var construtor in construtores)
+    {
+        Console.WriteLine($"Construtor: {construtor.Name}");
+        Console.WriteLine($"  No. de parâmetros: {construtor.GetParameters().Length}");
+    }
 }
